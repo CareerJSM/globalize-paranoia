b401bafcee7f61d4ffd8d9e4a7d62db12aa147c9
jefvlamings <vlamingsjef@gmail.com>
2018-11-06 14:30:49 +0100

    Rails 5.2 compatibility

--- lib/globalize/paranoia.rb
+++ lib/globalize/paranoia.rb
@@ -6,9 +6,10 @@ Globalize::ActiveRecord::ActMacro.module_eval do
     setup_translates_without_paranoia!(options)
 
     if options[:paranoia] && translation_class.attribute_names.include?('deleted_at')
-      translation_class.send(:acts_as_paranoid)
+      translation_class.send(:acts_as_paranoid, without_default_scope: true)
     end
   end
 
-  alias_method_chain :setup_translates!, :paranoia
+  alias_method :setup_translates_without_paranoia!, :setup_translates!
+  alias_method :setup_translates!, :setup_translates_with_paranoia!
 end
